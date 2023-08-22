7e5e83232ba725e97738ecadaaca41526950b783
Microvolts1 <vuivatva305@gmail.com>
2023-08-22 20:30:08 +0700

    Adding clerk and route basic setup

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,9 @@
-import Image from 'next/image'
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <p>Hello World!</p>
+    <div className="p-4">
+      <UserButton afterSignOutUrl="/"/>
+    </div>
   )
 }
