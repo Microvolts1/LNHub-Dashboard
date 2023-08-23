7322f1dbc44fa76f5d915bce4ed64fde0f5ade09
Microvolts1 <vuivatva305@gmail.com>
2023-08-23 08:27:22 +0700

    Complete story modal

--- components/modals/story-modal.tsx
+++ components/modals/story-modal.tsx
@@ -1,19 +1,64 @@
-'use client';
+"use client";
+
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 import { useStoryModal } from "@/hooks/use-story-modal";
 import { Modal } from "../ui/modal";
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "../ui/form";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+
+const formSchema = z.object({
+  name: z.string().min(1),
+});
 
 export const StoryModal = () => {
   const storyModal = useStoryModal();
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+    },
+  });
+
+  const onSubmit = async (value: z.infer<typeof formSchema>) => {
+    // TODO: Create Story.
+  };
 
   return (
     <Modal
-      title='Create a new story'
-      description="What will it be name this time?"
+      title="Create a new story."
+      description="Add a new story that you want to share."
       isOpen={storyModal.isOpen}
       onClose={storyModal.onClose}
     >
-      Future create story Form
+      <div>
+        <div className="space-y-4 py-2 pb-4">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <FormField
+                control={form.control}
+                name="name"
+                render={({field}) => (
+                  <FormItem>
+                    <FormLabel>Name</FormLabel>
+                    <FormControl>
+                      <Input placeholder="What will be it name this time?" {...field}/>
+                    </FormControl>
+                    <FormMessage/>
+                  </FormItem>
+                )}
+              />
+              <div className="pt-6 space-x-2 flex items-center justify-end w-full">
+                <Button variant='outline' onClick={storyModal.onClose}>Cancel</Button>
+                <Button type="submit">Continue</Button>
+              </div>
+            </form>
+          </Form>
+        </div>
+      </div>
     </Modal>
-  )
-}
+  );
+};
