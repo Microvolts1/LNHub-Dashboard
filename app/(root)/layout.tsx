265c215facc102515418932a661a3446e4b44047
Microvolts1 <vuivatva305@gmail.com>
2023-08-25 08:13:01 +0700

    Update layout for root

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,28 @@
+import prismadb from "@/lib/prismadb";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+export default async function SetupLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const { userId } = auth();
+
+  if (!userId) {
+    redirect("/sign-in");
+  }
+
+  const story = await prismadb.story.findFirst({
+    where: {
+      userId,
+    },
+    orderBy: {
+      updatedAt: 'desc'
+    }
+  });
+
+  if (story) {
+    redirect(`/${story.id}`);
+  }
+}
