99a9fb7afae814710011be848664ae11f9639f7e
Microvolts1 <vuivatva305@gmail.com>
2023-08-25 08:38:08 +0700

    Add main-nav in navbar, settings route is a template

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,22 +1,21 @@
-import { UserButton } from '@clerk/nextjs'
-import React from 'react'
+import { UserButton } from "@clerk/nextjs";
+
+import { MainNav } from "./main-nav";
 
 const Navbar = () => {
   return (
-    <div className='border-b'>
-      <div className='flex h-16 items-center px-4'>
+    <div className="border-b">
+      <div className="flex h-16 items-center px-4">
+        <div>This will be a story switcher</div>
         <div>
-          This will be a story switcher
+          <MainNav className="mx-6"/>
         </div>
-        <div>
-          This will be the routes
-        </div>
-        <div className='ml-auto flex items-center space-x-4'>
-          <UserButton afterSignOutUrl='/'/>
+        <div className="ml-auto flex items-center space-x-4">
+          <UserButton afterSignOutUrl="/" />
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
