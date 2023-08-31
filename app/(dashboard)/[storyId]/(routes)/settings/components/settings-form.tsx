4b7c06a2294f1f3641faa58bd1fa33a780bbf348
Microvolts1 <vuivatva305@gmail.com>
2023-08-31 20:35:28 +0700

    Update setting-form structure

--- app/(dashboard)/[storyId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storyId]/(routes)/settings/components/settings-form.tsx
@@ -4,14 +4,22 @@ import { Story } from "@prisma/client";
 import * as z from "zod";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
 
 import { Button } from "@/components/ui/button";
 import Heading from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
 
 import { Trash } from "lucide-react";
-
-
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 
 interface SettingsFormProps {
   initialData: Story;
@@ -24,10 +32,17 @@ const formSchema = z.object({
 type SettingsFormValue = z.infer<typeof formSchema>;
 
 const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
+  const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+
   const form = useForm<SettingsFormValue>({
     resolver: zodResolver(formSchema),
-    defaultValues: initialData
+    defaultValues: initialData,
   });
+
+  const onSubmit = async (data: SettingsFormValue) => {
+    //submit data
+  };
 
   return (
     <>
@@ -38,6 +53,35 @@ const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
         </Button>
       </div>
       <Separator />
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-8 w-full"
+        >
+          <div className="grid grid-cols-3 gap-8">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={loading}
+                      placeholder="Story name"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <Button disabled={loading} className="ml-auto" typeof="submit">
+            Save Changes
+          </Button>
+        </form>
+      </Form>
     </>
   );
 };
