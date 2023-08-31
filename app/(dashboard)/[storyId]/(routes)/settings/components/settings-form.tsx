82815b567b3cb90a8142bcf2b614e13fb03c2df8
Microvolts1 <vuivatva305@gmail.com>
2023-08-31 20:20:14 +0700

    Define form with zod

--- app/(dashboard)/[storyId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storyId]/(routes)/settings/components/settings-form.tsx
@@ -1,16 +1,34 @@
 "use client";
 
+import { Story } from "@prisma/client";
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
 import { Button } from "@/components/ui/button";
 import Heading from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
-import { Story } from "@prisma/client";
+
 import { Trash } from "lucide-react";
+
+
 
 interface SettingsFormProps {
   initialData: Story;
 }
 
+const formSchema = z.object({
+  name: z.string().min(1),
+});
+
+type SettingsFormValue = z.infer<typeof formSchema>;
+
 const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
+  const form = useForm<SettingsFormValue>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialData
+  });
+
   return (
     <>
       <div className="flex items-center justify-between">
@@ -19,7 +37,7 @@ const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
           <Trash className="h-4 w-4" />
         </Button>
       </div>
-      <Separator/>
+      <Separator />
     </>
   );
 };
