2dca7d952095a0629ee37df78712c5ae633d8f2f
Microvolts1 <vuivatva305@gmail.com>
2023-08-31 20:04:41 +0700

    Adding base settings page and settings form

---
+++ app/(dashboard)/[storyId]/(routes)/settings/page.tsx
@@ -0,0 +1,38 @@
+import prismadb from "@/lib/prismadb";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import SettingsForm from "./components/settings-form";
+
+interface SettingsPageProps {
+  params: {
+    storyId: string;
+  };
+}
+
+const SettingsPage: React.FC<SettingsPageProps> = async ({ params }) => {
+  const { userId } = auth();
+  if (!userId) {
+    redirect("/sign-in");
+  }
+
+  const story = await prismadb.story.findFirst({
+    where: {
+      id: params.storyId,
+      userId,
+    },
+  });
+
+  if (!story) {
+    redirect("/");
+  }
+
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <SettingsForm />
+      </div>
+    </div>
+  );
+};
+
+export default SettingsPage;
