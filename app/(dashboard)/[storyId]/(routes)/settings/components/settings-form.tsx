f242664f09c605a63d5e221fd988b2697d99201b
Microvolts1 <vuivatva305@gmail.com>
2023-08-31 20:16:23 +0700

    Update setting-form page

--- app/(dashboard)/[storyId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storyId]/(routes)/settings/components/settings-form.tsx
@@ -1,7 +1,8 @@
-'use client';
+"use client";
 
 import { Button } from "@/components/ui/button";
 import Heading from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
 import { Story } from "@prisma/client";
 import { Trash } from "lucide-react";
 
@@ -9,22 +10,18 @@ interface SettingsFormProps {
   initialData: Story;
 }
 
-const SettingsForm: React.FC<SettingsFormProps> = ({initialData}) => {
+const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
   return (
-    <div className="flex items-center justify-between">
-      <Heading
-        title="Settings"
-        description="Manage your story preferences"
-      />
-      <Button
-        variant='destructive'
-        size='icon'
-        onClick={() => {}}
-      >
-        <Trash className="h-4 w-4"/>
-      </Button>
-    </div>
-  )
-}
+    <>
+      <div className="flex items-center justify-between">
+        <Heading title="Settings" description="Manage your story preferences" />
+        <Button variant="destructive" size="icon" onClick={() => {}}>
+          <Trash className="h-4 w-4" />
+        </Button>
+      </div>
+      <Separator/>
+    </>
+  );
+};
 
-export default SettingsForm
+export default SettingsForm;
