738f9fe6ce5791a1cb0e18ae7c3dd77497e5837f
Microvolts1 <vuivatva305@gmail.com>
2023-09-01 14:35:13 +0700

    Add dark mode button with install of dropdown menu from shadcn-ui

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,6 +4,7 @@ import { redirect } from "next/navigation";
 import { MainNav } from "./main-nav";
 import StorySwitcher from "./story-switcher";
 import prismadb from "@/lib/prismadb";
+import { ModeToggle } from "./ui/dark-mode-button";
 
 const Navbar = async () => {
   const { userId } = auth();
@@ -23,6 +24,7 @@ const Navbar = async () => {
         <StorySwitcher items={stories}/>
         <MainNav className="mx-6" />
         <div className="ml-auto flex items-center space-x-4">
+          <ModeToggle/>
           <UserButton afterSignOutUrl="/" />
         </div>
       </div>
